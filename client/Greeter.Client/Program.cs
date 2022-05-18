c98d2809db2fe17cf453fd007f50ed3ff93654bd
Dean Herringer <deanher@gmail.com>
2022-05-18 18:31:24 +0200

    initial commit

---
+++ client/Greeter.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+Console.WriteLine("Hello, World!");
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7108");
+var client = new Greeter.GreeterClient(channel);
+
+Console.Write("Type a name and press Enter: ");
+var name = Console.ReadLine();
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+
+Console.WriteLine($"Greeting: {reply.Message}");
+
+Console.Write("Type a greeting and press Enter: ");
+var goodbyeGreeting = Console.ReadLine();
+Console.Write("Type a name and press Enter: ");
+var goodbyeName = Console.ReadLine();
+var goodbyeReply = await client.SayGoobyeAsync(new GoodbyeRequest { Greeting = goodbyeGreeting, Name = goodbyeName });
+
+Console.WriteLine($"Goodbye Greeting: {goodbyeReply.Message}");
+
+Console.WriteLine("Press any key to exit...");
+
+Console.ReadKey();
